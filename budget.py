eec09c0f53fddc72f96b4d60192f1481c4f1df2c
Marc Sota <atoscram@yahoo.com>
2023-05-12 09:26:09 +1000

    cleaned up code and added comments

--- budget.py
+++ budget.py
@@ -5,6 +5,8 @@ class Category:
         self.balance = float()
 
     def __str__(self):
+        # Override the super method for when printing out the object
+        # Returns ledger list
         print_ledger = str()
         print_ledger += f"{self.name.center(30, '*')}\n"
 
@@ -16,6 +18,7 @@ class Category:
         return print_ledger
 
     def deposit(self, amount, description=""):
+        # add amount to balance and ledger
         self.balance += amount
         self.ledger.append({
             "amount": amount,
@@ -23,6 +26,8 @@ class Category:
         })
 
     def withdraw(self, amount, description=""):
+        # subtract amount from balance and add negative amount to ledger
+        # Returns success boolean
         if self.check_funds(amount):
             self.balance -= amount
             self.ledger.append({
@@ -37,6 +42,8 @@ class Category:
         return self.balance
 
     def transfer(self, amount, category):
+        # move amount from one category to another if there are enough funds
+        # Returns success boolean
         if self.check_funds(amount):
             self.withdraw(amount, f"Transfer to {category.name}")
             category.deposit(amount, f"Transfer from {self.name}")
@@ -45,30 +52,35 @@ class Category:
             return False
 
     def check_funds(self, amount):
+        # Returns boolean if there is enough money in balance
         if amount > self.balance:
             return False
         else:
             return True
 
+
 def create_spend_chart(categories):
+    # Returns bar graph of percentage spent per category
     if len(categories) > 4:
         print("Max 4 categories")
     else:
         percentage_per_category = dict()
 
         for category in categories:
+            # List comprehension of getting the total withdrawals and deposits per category
             total_withdrawals = sum(abs(item['amount']) for item in category.ledger if item['amount'] < 0)
             total_deposits = sum(abs(item['amount']) for item in category.ledger if item['amount'] > 0)
 
+            # Calculate percentage spent on each category and add it to a dictionary
             percentage_spent = (total_withdrawals / total_deposits) * 100
-
             percentage_per_category[category.name] = percentage_spent
 
+        # Building the bar chat
         bar_chart = str()
         bar_chart += f"Percentage spent by category\n"
-
         percentage_max = 100
 
+        # Building the bar percentage
         while percentage_max > -1:
             bar_chart += f"{str(percentage_max).rjust(3)}|"
 
@@ -81,8 +93,9 @@ def create_spend_chart(categories):
             percentage_max -= 10
             bar_chart += " \n"
 
-        bar_chart += f"    {''.ljust(3*len(categories)+1, '-')}"
+        bar_chart += f"    {''.ljust(3 * len(categories) + 1, '-')}"
 
+        # Building the vertical category names
         for i in range(0, max([len(category.name) for category in categories])):
             bar_chart += "\n    "
             for category in categories:
